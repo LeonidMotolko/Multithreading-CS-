fe01b97170db3e2a205d7c3ee51e7c64d60afae6
seachran <motolko.leonid.1980@gmail.com>
2025-06-24 14:08:22 +0300

    make 1st task

---
+++ Lab3/Task1/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using TablesLibrary;
+using MyProject.Services;
+
+namespace MyProject
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var manufacturers = new List<Manufacturer>();
+            for (int i = 1; i <= 20; i++)
+            {
+                manufacturers.Add(Manufacturer.Create(
+                    $"Manufacturer{i}",
+                    $"Address{i}",
+                    i % 2 == 0));
+            }
+
+            var serializer = new ParallelSerializer();
+
+            var firstBatch = manufacturers.GetRange(0, 10);
+            var secondBatch = manufacturers.GetRange(10, 10);
+
+            Console.WriteLine("Starting parallel serialization...");
+            await serializer.ParallelSerializeAsync(
+                firstBatch, "manufacturers1.json",
+                secondBatch, "manufacturers2.json");
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
---
+++ Lab3/Task1/Task1/Services/ParallelSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization.Json;
+
+namespace MyProject.Services
+{
+    public class ParallelSerializer
+    {
+        public async Task SerializeToFileAsync<T>(List<T> objects, string filePath)
+        {
+            await Task.Run(() =>
+            {
+                var serializer = new DataContractJsonSerializer(typeof(List<T>));
+                using (var stream = File.Create(filePath))
+                {
+                    serializer.WriteObject(stream, objects);
+                }
+            });
+        }
+
+        public async Task ParallelSerializeAsync<T>(List<T> firstBatch, string firstFile,
+                                                 List<T> secondBatch, string secondFile)
+        {
+            var task1 = SerializeToFileAsync(firstBatch, firstFile);
+            var task2 = SerializeToFileAsync(secondBatch, secondFile);
+
+            await Task.WhenAll(task1, task2);
+        }
+    }
+}
